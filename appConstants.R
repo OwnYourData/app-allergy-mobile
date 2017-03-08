bed207aabed6dbcf77ce7149e5513cf7d6aa30c6
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-03-08 16:25:24 +0100

    initial commit

---
+++ appConstants.R
@@ -0,0 +1,7 @@
+# global constants available to the mobile app
+# last update:2016-10-17
+
+# constants required for every app
+appTitle <- 'Allergie-Tagebuch'
+app_id <- 'eu.ownyourdata.allergy'
+desktopUrl <- 'https://allergie-tagebuch.datentresor.org'
